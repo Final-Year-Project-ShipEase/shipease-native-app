6c0a795149f1c0eb6ceaf625c104664f10634cf9
aminhas309@hotmail.com <hamzaidrees083@gmail.com>
2024-03-21 12:27:53 +0500

    SplashScreen & OnBoardingScreens

--- App.js
+++ App.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import HomeScreen from './src/component/homeScreen';
+import SplashScreen from './src/User/splashScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ThemeProvider } from '@mui/material';
 import theme from './theme';
+import OnBoardingScreenOne from './src/User/onBoardingScreen/onBoardingScreenOne';
+import OnBoardingScreenTwo from './src/User/onBoardingScreen/onBoardingScreenTwo';
+import OnBoardingScreenThree from './src/User/onBoardingScreen/onBoardingScreenThree';
+import LoginScreen from './src/User/loginScreen/loginScreen';
+import SignUpScreen from './src/User/signUpScreen/signUpScreen';
+import OPTVerification from './src/User/component/OtpScreen/otpVerification';
+import VerificationSuccessfulModal from './src/User/modal/verificationSuccessfulModal';
+import { Button } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme} >
+    <ThemeProvider theme={theme}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+          <Stack.Group screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="SplashScreen" component={SplashScreen} />
+            <Stack.Screen
+              name="OnBoardingScreenOne"
+              component={OnBoardingScreenOne}
+            />
+            <Stack.Screen
+              name="OnBoardingScreenTwo"
+              component={OnBoardingScreenTwo}
+            />
+            <Stack.Screen
+              name="OnBoardingScreenThree"
+              component={OnBoardingScreenThree}
+            />
+          </Stack.Group>
+
+          <Stack.Group
+            screenOptions={{ headerShown: false, headerTitleAlign: 'center' }}
+          >
+            <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+            <Stack.Screen name="OTPVerification" component={OPTVerification} />
+
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen
+              name="VerificationSuccessfulModal"
+              component={VerificationSuccessfulModal}
+            />
+          </Stack.Group>
         </Stack.Navigator>
       </NavigationContainer>
     </ThemeProvider>
--- theme.js
+++ theme.js
@@ -1,17 +1,63 @@
-
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
-
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#00668C',
-        },
-        secondary: {
-            main: '#71C4EF',
-            hover: '#629DBC',
-        },
+  palette: {
+    primary: {
+      main: '#01668c',
     },
+    secondary: {
+      main: '#71C4EF',
+      hover: '#629DBC',
+    },
+    button: {
+      backgroundColor: '#00000026',
+      textColor: '#000000',
+    },
+    alreadyAccount: {
+      textColor: '#FFFFFF',
+    },
+    card: {
+      backgroundColor:
+        'linear-gradient(219.14deg, rgba(0,102,140,1) 5.5%, rgba(0,102,140,1) 47.24%, rgba(43,143,180,1) 68.5%, rgba(88,184,221,1) 94.11%)',
+      titleColor: '#FFFFFF',
+      textColor: 'rgba(255, 255, 255, 0.8)',
+      buttonColor: 'rgba(113, 196, 239, 1)',
+    },
+    registration: {
+      title: '#00668C',
+      content: '#292B2D',
+      backgroundColorRegistration: '#00668C',
+      textColorRegistration: '#FFFFFF',
+      backgroundColorLogin: '#00000026',
+      textColorLogin: '#00668C',
+    },
+    login: {
+      backgroundColorLogin: '#00668C',
+      textColorLogin: '#FCFCFC',
+      backgroundColorRegistration: '#2C14DD',
+      textColorRegistration: '#00668C',
+    },
+
+    otpVerification: {
+      outerRoundView: '#abcdd9',
+      innerRoundView: '#00668c',
+      titleContentColor: '#00668C',
+      bodyContentColor: '#75787E',
+      otpCode: '#00668c',
+      buttonText: '#00668C',
+    },
+
+    verificationSuccessfulModal: {
+      backgroundColor: '#01668c',
+      checkDone: '#0f6f93',
+      outerViewColor: '#267d9d',
+      innerViewColor: '#ffffff',
+      modalBodyText: '#E6E6E6',
+      modalTitleText: '#E6E6E6',
+      buttonColor: '#FFFFFF',
+      buttonTextColor: '#000000',
+    },
+  },
 });
 responsiveFontSizes(theme);
 
