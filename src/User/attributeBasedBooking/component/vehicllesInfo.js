cef4050f3ca4e1f8ce44282bb9520edf84b9c402
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-03-26 00:51:52 +0500

    Vehicles Info Component

--- src/User/attributeBasedBooking/component/vehicllesInfo.js
+++ src/User/attributeBasedBooking/component/vehicllesInfo.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   View,
   Text,
   Image,
   Dimensions,
+  Pressable,
 } from 'react-native';
 import theme from '../../../../theme';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 const VehiclesInfo = () => {
+    const [isSolid, setIsSolid] = useState(false);
+
+    const toggleHeart = () => {
+      setIsSolid((prev) => !prev); // Toggle the state
+    };
 
 return (
     <View style={[styles.pages]}>
@@ -18,31 +25,42 @@ return (
           <View style={[styles.buttons4, styles.buttonsPosition]}>
             <Text style={styles.addNew4}>Available</Text>
           </View>
-          <View style={[styles.jumboTrailerParent, styles.parentPosition]}>
-            <Text style={[styles.jumboTrailer, styles.boxTruckTypo]}>
+          <View style={[styles.TypeParent, styles.parentPosition]}>
+            <Text style={[styles.Type, styles.TruckTypo]}>
               JUMBO TRAILER
             </Text>
-            <View style={styles.faisalabadParent}>
+            <View style={styles.cityparent}>
               <Text
-                style={[styles.faisalabad, styles.faisalabadTypo]}
-              >{`Faisalabad  `}</Text>
-              <Image
-                style={[styles.groupChild, styles.groupChildPosition1]}
-                contentFit="cover"
-                source={require("./dot.png")}
-              />
-              <Text style={[styles.tbf7393, styles.r521Typo]}>TBF7393</Text>
-              <Image
+                style={[styles.city, styles.cityTypo]}
+              >{`faisalabad  `}</Text>
+                <Image
                 style={[styles.groupItem, styles.groupPosition]}
                 contentFit="cover"
                 source={require("./dot.png")}
               />
-              <Text style={[styles.r521, styles.r521Typo]}>R-521</Text>
+              <Text style={[styles.trackerno, styles.registrationTypo]}>TBF7393</Text>
+                <Image
+                style={[styles.groupChild, styles.groupChildPosition1]}
+                contentFit="cover"
+                source={require("./dot.png")}
+              />
+              <Text style={[styles.registrationNo, styles.registrationTypo]}>R-521</Text>
             </View>
           </View>
+          <Pressable onPress={toggleHeart}>
+          <View style={styles.heartIcon}>
+                <Text>
+                {isSolid ? (
+                    <Icon name="heart" size={25} style={[styles.heart]} />
+                ) : (
+                    <Icon name="heart-o" size={25} color="black" />
+                )}
+                </Text>
+            </View>
+            </Pressable>
         </View>
         <Image
-          style={[styles.stockPhotoEindhovenTheNethIcon, styles.iconLayout]}
+          style={[styles.VehicleImage, styles.iconLayout]}
           contentFit="cover"
           source={require("./vehicleimage.png")}
         />
@@ -60,51 +78,52 @@ const styles = StyleSheet.create({
     },
 
     truckComponent: {
-        top: 201,
-        height: 87,
-        left: 29,
-        width: 316,
+        top: height * 0.03,
+        height: height * 0.2,
+        left: width * 0.07,
+        width: width * 0,
       },
 
       truckLayout1: {
-        width: 316,
-        height: 87,
+        width: width * 0.8,
+        height: height * 0.05,
         position: "absolute",
       },
 
       textArea: {
-        width: "64.87%",
-        left: "35.13%",
-        right: "0%",
-        bottom: "13.22%",
-        top: "14.94%",
-        height: "71.84%",
+        width:width * 0.1,
+        left: width * 0.29,
+        right: width * 0,
+        bottom: height * 0.3,
+        top: height * 0.1,
+        height: height * 0.3,
         position: "absolute",
       },
 
       textPosition: {
-        bottom: "13.22%",
-        top: "14.94%",
-        height: "71.84%",
+        bottom: height * 0.3,
+        top:  height * 0.01,
+        height: height * 0.3,
       },
 
       addNew4: {
-        fontSize: 14,
+        fontSize: height * 0,
         textAlign: "left",
         color: theme.palette.alreadyAccount.textColor,
       },
       buttons4: {
-        width: "30.73%",
-        right: "69.27%",
-        left: "0%",
+        width: width * 0.12,
+        right: width * 0.1,
+        left: width * 0.001,
       },
 
       buttonsPosition: {
-        backgroundColor:theme.palette.dashboard.mainblue,
-        borderRadius: 14,
-        bottom: "60%",
-        top: "8%",
-        height: "32%",
+        backgroundColor:theme.palette.dashboard.lightblue,
+        borderRadius: width * 0.043,
+        bottom: height * 0.1,
+        top: height * 0,
+        height: height * 0.04,
+        width: width * 0.22,
         padding: 10,
         justifyContent: "center",
         alignItems: "center",
@@ -113,93 +132,93 @@ const styles = StyleSheet.create({
       },
 
       parentPosition: {
-        top: "44.8%",
-        height: "55.2%",
-        bottom: "0%",
+        top: height * 0.045,
+        height: height * 0.045,
+        bottom:height * 0,
       },
 
-      jumboTrailerParent: {
-        width: "92.15%",
-        right: "7.85%",
-        left: "0%",
+      TypeParent: {
+        width: width * 0.4,
+        right: width * 0.17,
+        left: width * 0,
         position: "absolute",
       },
 
-      boxTruckTypo: {
-        color: theme.palette.dashboard.mainblue,
-        fontSize: 14,
+      TruckTypo: {
+        color: theme.palette.dashboard.black,
+        fontSize: height * 0.018,
         textAlign: "left",
-        top: "0%",
-        height: "100%",
+        top: height * 0,
+        height: height,
         fontWeight: "700",
       },
 
-      jumboTrailer: {
-        left: "0%",
+      Type: {
+        left: width * 0.007,
         position: "absolute",
-        width: "100%",
+        width: width,
       },
 
-      faisalabadParent: {
-        width: "80.47%",
-        right: "19.53%",
-        bottom: "7.25%",
-        top: "49.28%",
-        height: "43.48%",
-        left: "0%",
-        position: "absolute",
-      },
-
-      faisalabad: {
-        width: "43.42%",
-        left: "0%",
+      cityparent: {
+        width: width * 0.5,
+        right: width * 0.12,
+        bottom: height * 0.12,
+        top: height * 0.024,
+        height: height * 0.02,
+        left: width * 0.01,
         position: "absolute",
       },
 
-      faisalabadTypo: {
-        color: theme.palette.dashboard.mainblue,
-        fontSize: 14,
+      city: {
+        width: width,
+        left: width * 0,
+        position: "absolute",
+      },
+
+      cityTypo: {
+        color: theme.palette.dashboard.black,
+        fontSize: height * 0.017,
         textAlign: "left",
-        top: "0%",
-        height: "100%",
-        fontWeight: "300",
+        top: height * 0,
+        height: height,
+        fontWeight: "400",
       },
 
       groupChild: {
-        right: "29.61%",
-        left: "69.08%",
-        width: "1.32%",
-        height: "13.33%",
+        right: width * 0.02,
+        left: width * 0.352,
+        width: width * 0.008,
+        height: height * 0.003,
         maxHeight: "100%",
         maxWidth: "100%",
         position: "absolute",
         overflow: "hidden",
       },
 
-      tbf7393: {
-        left: "41.45%",
+      trackerno: {
+        left: width * 0.19,
       },
 
-      r521Typo: {
-        width: "26.97%",
-        color: theme.palette.dashboard.mainblue,
-        fontSize: 14,
+      registrationTypo: {
+        width:width*0.17,
+        color: theme.palette.dashboard.black,
+        fontSize: height *  0.017,
         textAlign: "left",
-        top: "0%",
-        height: "100%",
-        fontWeight: "300",
+        top: height * 0,
+        height: height,
+        fontWeight: "400",
         position: "absolute",
       },
 
-      r521: {
-        left: "73.03%",
+      registrationNo: {
+        left: height * 0.17,
       },
 
       groupItem: {
-        right: "59.87%",
-        left: "38.82%",
-        width: "1.32%",
-        height: "13.33%",
+        right: width * 0.02,
+        left: width * 0.18,
+        width: width * 0.008,
+        height: height * 0.003,
         maxHeight: "100%",
         maxWidth: "100%",
         position: "absolute",
@@ -212,8 +231,8 @@ const styles = StyleSheet.create({
       },
 
       groupChildPosition1: {
-        bottom: "46.67%",
-        top: "40%",
+        bottom: height * 0.2,
+        top: height * 0.01,
       },
 
       islamabadParentPosition: {
@@ -233,45 +252,30 @@ const styles = StyleSheet.create({
         position: "absolute",
       },
 
-      stockPhotoEindhovenTheNethIcon1: {
-        right: "70.25%",
-        width: "29.75%",
-        borderRadius: 14,
-        maxHeight: "100%",
-        maxWidth: "100%",
-        left: "0%",
-        bottom: "0%",
-        top: "0%",
-        height: "100%",
+      VehicleImage: {
+        right: width * 0.1,
+        width: width * 0.26,
+        borderRadius: width * 0.5,
+        left: width * 0.0005,
+        bottom: height * 0,
+        top: height * 0,
+        height: height * 0.11,
         overflow: "hidden",
-        display: "none",
       },
 
       iconLayout: {
-        borderRadius: 14,
+        borderRadius: width * 0.04,
         position: "absolute",
       },
 
-      truckComponent51: {
-        height: 87,
-        top: 0,
-        left: 0,
+      heart: {
+        colo:theme.palette.dashboard.mainblue,
       },
-      image11Icon: {
-        width: 94,
-        height: 87,
-        top: 0,
-        left: 0,
-      },
-      truckComponent5: {
-        top: 492,
-        height: 87,
-        left: 29,
-        width: 316,
-      },
-      buttons6: {
-        width: 63,
-        left: 0,
+
+      heartIcon:{
+        position: 'relative',
+        left: width * 0.47,
+        top: height * 0
       },
 
 });
